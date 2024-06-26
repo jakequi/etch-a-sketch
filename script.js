a5f1eb1009e6c4671c9be55394bd15772796163d
Jake <jgoudiehargreaves@gmail.com>
2024-06-26 01:02:45 +0100

    Added more sketchpad functionality

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+let colourSelect;
+let sketchDivs;
+let buttonState = "draw";
+let mouseDown = false;
+const drawButton = document.querySelector(".draw-button");
+const eraseButton = document.querySelector(".erase-button");
+const colourPicker = document.querySelector(".colour-picker");
+const gridReset = document.querySelector(".grid-reset");
+const gridSize = document.querySelector(".grid-size");
+const holdClickMode = document.querySelector(".hold-click-mode")
+const rainbowColours = document.querySelector(".rainbow-mode")
+const gridVisible = document.querySelector(".grid-visible")
+
 function createGrid(num) {
     const container = document.querySelector(".container");
     container.textContent = "";
@@ -5,22 +18,51 @@ function createGrid(num) {
         const newDiv = document.createElement("div");
         newDiv.setAttribute("class", "sketch-grid");
         newDiv.style.flexBasis = `${100 / num}%`;
-        newDiv.dataset.hoverCount = 0;  // Initialize hover count
         container.appendChild(newDiv);
+        colourSelect = "black";
     }
-    const sketchDivs = document.querySelectorAll(".sketch-grid");
+    sketchDivs = document.querySelectorAll(".sketch-grid");
     sketchDivs.forEach(sketchDiv => {
         sketchDiv.addEventListener("mouseenter", () => {
-            let hoverCount = Number(sketchDiv.dataset.hoverCount);
-            if (!sketchDiv.style.backgroundColor) {
-                sketchDiv.style.backgroundColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-            } 
+            if (mouseDown) {
+                if (buttonState === "erase") {
+                    sketchDiv.style.backgroundColor = "";
+                }
+                else if (!sketchDiv.style.backgroundColor) {
+                    if (rainbowColours.checked) {
+                        sketchDiv.style.backgroundColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+                    }
+                    else{
+                        sketchDiv.style.backgroundColor = colourSelect;
+                    }
+                } 
+            }
         });
     });
+    gridVisible.checked = true;
 }
 
-const newGridButton = document.querySelector("button");
-newGridButton.addEventListener("click", () => {
+createGrid(32);
+
+drawButton.addEventListener("click", () => {
+    drawButton.classList.add("toggled-button");
+    eraseButton.classList.remove("toggled-button")
+    buttonState = "draw";
+});
+
+eraseButton.addEventListener("click", () => {
+    drawButton.classList.remove("toggled-button");
+    eraseButton.classList.add("toggled-button")
+    buttonState = "erase";
+});
+
+colourPicker.addEventListener("change", () => {
+    if (!rainbowColours.checked) {
+        colourSelect = colourPicker.value;
+    }
+});
+
+gridSize.addEventListener("click", () => {
     let newNum = prompt("Please enter the number of squares you want along each axis");
     while (!Number.isInteger(Number(newNum)) || newNum > 100 || newNum < 1) {
         newNum = prompt("Please enter a number between 1 and 100");
@@ -28,4 +70,42 @@ newGridButton.addEventListener("click", () => {
     createGrid(newNum);
 });
 
-createGrid(16);
+gridReset.addEventListener("click", () => {
+    const sketchDivs = document.querySelectorAll(".sketch-grid");
+    sketchDivs.forEach(sketchDiv => {
+        sketchDiv.style.backgroundColor = "";
+    });
+});
+
+rainbowColours.addEventListener("change", () => {
+    if (!rainbowColours.checked) {
+        colourSelect = colourPicker.value;
+    }
+});
+
+gridVisible.addEventListener("change", () => {
+    sketchDivs.forEach(sketchDiv => {
+        sketchDiv.classList.toggle("sketch-grid-no-border");
+    });
+});
+
+holdClickMode.addEventListener("change", () => {
+    if(holdClickMode.checked) {
+        mouseDown = false;
+    }
+    else {
+        mouseDown = true;
+    }
+})
+
+document.addEventListener("mousedown", () => {
+    if (holdClickMode.checked) {
+        mouseDown = true;
+    }
+});
+
+document.addEventListener("mouseup", () => {
+    if (holdClickMode.checked) {
+        mouseDown = false;
+    }
+});
